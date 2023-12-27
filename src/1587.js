d3615a1450ea5af09173fc43144cb10ddb050c79
lizunlong <lizunlong@gmail.com>
2023-12-27 14:04:59 +0800

    Update 1587.js

--- src/1587.js
+++ src/1587.js
@@ -10,15 +10,15 @@ function calculateMinJumps(arr) {
     const item = arr[idx];
 
     if (idx + item >= arr.length - 1) {
-      jump++
-      break
+      jump++;
+      break;
     }
 
     for (let i = 1; i <= item; i++) {
       const num = arr[idx + i];
       if (num > max) {
-        max = num
-        maxIdx = idx + i
+        max = num;
+        maxIdx = idx + i;
       }
     }
     idx = maxIdx;
@@ -30,3 +30,29 @@ function calculateMinJumps(arr) {
 }
 
 console.log(calculateMinJumps(stepArr));
+
+function minJumps(arr) {
+  let jumps = 0,
+    currentEnd = 0,
+    farthest = 0;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    // Update the farthest we can reach
+    farthest = Math.max(farthest, i + arr[i]);
+
+    // If we have come to the end of the current range
+    if (i === currentEnd) {
+      jumps++;
+      currentEnd = farthest;
+
+      // If the farthest we can reach is less than or equal to current index, jump is not possible
+      if (farthest <= i) return -1;
+    }
+  }
+
+  return jumps;
+}
+
+// Test the function with the provided array
+const testArray = [6, 2, 4, 0, 5, 1, 1, 4, 2, 9];
+console.log(minJumps(testArray)); // Should return 2
