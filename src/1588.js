09c2e8e9ce0c377c0426856deced67f146e30b4b
lizunlong <lizunlong@gmail.com>
2023-12-28 13:34:24 +0800

    Update 1588.js

--- src/1588.js
+++ src/1588.js
@@ -37,106 +37,7 @@ const grid = [
 
 console.log(countPaths(grid)); // Output should be 2
 
-// const example = [
-//   [0, 0, 1],
-//   [0, 0, 1],
-//   [1, 0, 0],
-// ];
-
-// function calculateWays(matrix) {
-//   let n = matrix[0].length;
-//   let m = matrix.length;
-
-//   let ways = 0;
-//   let ni = 0;
-//   let mi = 0;
-//   while (ni < n - 1 && mi < m - 1) {
-//     if (ni === n - 1 && mi === m - 1) {
-//       ways++;
-//       ni = 0;
-//       mi = 0;
-//     }
-
-//     if (matrix[mi][ni] === 1) {
-//       ni = 0;
-//       mi = 0;
-//     }
-
-//     if (matrix[mi][ni + 1] === 1) {
-//       mi++;
-//     }
-
-//     ni++;
-//     continue;
-//   }
-// }
-
-// function setMatrix(dp, mi, ni, val) {
-//   if (!dp[mi]) dp[mi] = [];
-//   if (!dp[mi][ni]) dp[mi][ni] = [];
-//   dp[mi][ni] = val;
-// }
-
-// function getVal(dp, mi, ni) {
-//   return dp?.[mi]?.[ni] ?? 0;
-// }
-
-// function method1(matrix) {
-//   const dp = [];
-
-//   let n = matrix[0].length;
-//   let m = matrix.length;
-//   let ni = 0;
-//   let mi = 0;
-//   while (ni < n && mi < m) {
-//     if (mi === 0 && ni === 0) {
-//       setMatrix(dp, mi, ni, 1);
-//       ni++;
-//       continue;
-//     }
-
-//     if (matrix[mi][ni] === 0) {
-//       if (mi > 0 && ni > 0) {
-//         setMatrix(dp, mi, ni, getVal(dp, mi - 1, ni), getVal(dp, mi, ni - 1));
-//       } else {
-//         setMatrix(dp, mi, ni, getVal(dp, mi, ni - 1));
-//       }
-//       ni++;
-//       continue;
-//     }
-
-//     mi++;
-//     ni--;
-//   }
-
-//   return dp;
-// }
-
-// console.log(method1(example));
-
-function dfs(arr, i = 0, j = 0, memo) {
-  if (!memo) {
-    memo = Array.from(Array(arr.length), () => Array(arr[0].length).fill(-1));
-  }
-
-  if (i === arr.length - 1 && j === arr[i].length - 1) {
-    return 1;
-  }
-
-  if (i > arr.length - 1 || j > arr[i].length - 1 || arr[i][j] === 1) {
-    return 0;
-  }
-
-  if (memo[i][j] !== -1) {
-    return memo[i][j];
-  }
-
-  memo[i][j] = dfs(arr, i + 1, j, memo) + dfs(arr, i, j + 1, memo);
-  return memo[i][j];
-}
-
-console.log(dfs(grid));
-
+// Method 2: Depth-First Search (DFS)
 function countPathsDFS(grid) {
   const N = grid.length;
   const M = grid[0].length;
@@ -167,3 +68,26 @@ function countPathsDFS(grid) {
 }
 
 console.log(countPathsDFS(grid)); // Output should be 2
+
+function dfs(arr, i = 0, j = 0, memo) {
+  if (!memo) {
+    memo = Array.from(Array(arr.length), () => Array(arr[0].length).fill(-1));
+  }
+
+  if (i === arr.length - 1 && j === arr[i].length - 1) {
+    return 1;
+  }
+
+  if (i > arr.length - 1 || j > arr[i].length - 1 || arr[i][j] === 1) {
+    return 0;
+  }
+
+  if (memo[i][j] !== -1) {
+    return memo[i][j];
+  }
+
+  memo[i][j] = dfs(arr, i + 1, j, memo) + dfs(arr, i, j + 1, memo);
+  return memo[i][j];
+}
+
+console.log(dfs(grid));
