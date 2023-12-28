39dceeb416cd1d5edd1b54e75f64ce4c162da9bc
lizunlong <lizunlong@gmail.com>
2023-12-28 12:11:43 +0800

    Update 1588.js

--- src/1588.js
+++ src/1588.js
@@ -1,3 +1,4 @@
+// Method 1: Dynamic Programming
 function countPaths(grid) {
   const N = grid.length;
   const M = grid[0].length;
@@ -36,79 +37,79 @@ const grid = [
 
 console.log(countPaths(grid)); // Output should be 2
 
-const example = [
-  [0, 0, 1],
-  [0, 0, 1],
-  [1, 0, 0],
-];
+// const example = [
+//   [0, 0, 1],
+//   [0, 0, 1],
+//   [1, 0, 0],
+// ];
 
-function calculateWays(matrix) {
-  let n = matrix[0].length;
-  let m = matrix.length;
+// function calculateWays(matrix) {
+//   let n = matrix[0].length;
+//   let m = matrix.length;
 
-  let ways = 0;
-  let ni = 0;
-  let mi = 0;
-  while (ni < n - 1 && mi < m - 1) {
-    if (ni === n - 1 && mi === m - 1) {
-      ways++;
-      ni = 0;
-      mi = 0;
-    }
+//   let ways = 0;
+//   let ni = 0;
+//   let mi = 0;
+//   while (ni < n - 1 && mi < m - 1) {
+//     if (ni === n - 1 && mi === m - 1) {
+//       ways++;
+//       ni = 0;
+//       mi = 0;
+//     }
 
-    if (matrix[mi][ni] === 1) {
-      ni = 0;
-      mi = 0;
-    }
+//     if (matrix[mi][ni] === 1) {
+//       ni = 0;
+//       mi = 0;
+//     }
 
-    if (matrix[mi][ni + 1] === 1) {
-      mi++;
-    }
+//     if (matrix[mi][ni + 1] === 1) {
+//       mi++;
+//     }
 
-    ni++;
-    continue;
-  }
-}
+//     ni++;
+//     continue;
+//   }
+// }
 
-function setMatrix(dp, mi, ni, val) {
-  if (!dp[mi]) dp[mi] = [];
-  if (!dp[mi][ni]) dp[mi][ni] = [];
-  dp[mi][ni] = val;
-}
+// function setMatrix(dp, mi, ni, val) {
+//   if (!dp[mi]) dp[mi] = [];
+//   if (!dp[mi][ni]) dp[mi][ni] = [];
+//   dp[mi][ni] = val;
+// }
 
-function getVal(dp, mi, ni) {
-  return dp?.[mi]?.[ni] ?? 0;
-}
+// function getVal(dp, mi, ni) {
+//   return dp?.[mi]?.[ni] ?? 0;
+// }
 
-function method1(matrix) {
-  const dp = [];
+// function method1(matrix) {
+//   const dp = [];
 
-  let n = matrix[0].length;
-  let m = matrix.length;
-  let ni = 0;
-  let mi = 0;
-  while (ni < n && mi < m) {
-    if (mi === 0 && ni === 0) {
-      setMatrix(dp, mi, ni, 1);
-      ni++;
-      continue;
-    }
+//   let n = matrix[0].length;
+//   let m = matrix.length;
+//   let ni = 0;
+//   let mi = 0;
+//   while (ni < n && mi < m) {
+//     if (mi === 0 && ni === 0) {
+//       setMatrix(dp, mi, ni, 1);
+//       ni++;
+//       continue;
+//     }
 
-    if (matrix[mi][ni] === 0) {
-      if (mi > 0 && ni > 0) {
-        setMatrix(dp, mi, ni, getVal(dp, mi - 1, ni), getVal(dp, mi, ni - 1));
-      } else {
-        setMatrix(dp, mi, ni, getVal(dp, mi, ni - 1));
-      }
-      ni++;
-      continue;
-    }
+//     if (matrix[mi][ni] === 0) {
+//       if (mi > 0 && ni > 0) {
+//         setMatrix(dp, mi, ni, getVal(dp, mi - 1, ni), getVal(dp, mi, ni - 1));
+//       } else {
+//         setMatrix(dp, mi, ni, getVal(dp, mi, ni - 1));
+//       }
+//       ni++;
+//       continue;
+//     }
 
-    mi++;
-    ni--;
-  }
+//     mi++;
+//     ni--;
+//   }
 
-  return dp;
-}
+//   return dp;
+// }
 
 // console.log(method1(example));
