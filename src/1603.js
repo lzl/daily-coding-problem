976de23a3475497c6b9c279a9bb1683b3b14c0ee
lizunlong <lizunlong@gmail.com>
2024-01-12 11:18:41 +0800

    Update 1603.js

--- src/1603.js
+++ src/1603.js
@@ -26,18 +26,23 @@ console.log(
 
 function countSetBits(N) {
   let totalSetBits = 0;
+  let bitPosition = 0;
 
-  for (
-    let bitPosition = 0;
-    bitPosition <= Math.floor(Math.log2(N));
-    bitPosition++
-  ) {
-    let step = 1 << (bitPosition + 1); // 2^(bitPosition + 1)
-    let fullCycleCount = Math.floor(N / step);
-    totalSetBits += fullCycleCount * (step >> 1); // fullCycleCount * (step / 2)
+  while (1 << bitPosition <= N) {
+    // 'bit' is the value of the bit in current bit position (either 0 or 1)
+    let bit = 0;
+    let change = 1 << bitPosition; // The interval after which the bit changes
 
-    let partialCycleLength = N % step;
-    totalSetBits += Math.max(0, partialCycleLength - (step >> 1));
+    for (let i = 0; i <= N; i++) {
+      totalSetBits += bit;
+      if (--change == 0) {
+        // When the counter goes down to 0, flip the bit and reset the counter
+        bit = !bit;
+        change = 1 << bitPosition;
+      }
+    }
+
+    bitPosition++;
   }
 
   return totalSetBits;
