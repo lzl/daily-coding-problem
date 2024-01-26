7e4e3c5723c8561a053dbda344f6e86aafba66d6
lizunlong <lizunlong@gmail.com>
2024-01-26 12:11:11 +0800

    Create 1617.js

---
+++ src/1617.js
@@ -0,0 +1,35 @@
+function simulateDominoes(dominoes) {
+  let changed;
+  do {
+    changed = false;
+    let newDominoes = dominoes.split("");
+
+    for (let i = 0; i < dominoes.length; i++) {
+      if (dominoes[i] === ".") {
+        if (
+          i > 0 &&
+          dominoes[i - 1] === "R" &&
+          (i === dominoes.length - 1 || dominoes[i + 1] !== "L")
+        ) {
+          newDominoes[i] = "R";
+          changed = true;
+        } else if (
+          i < dominoes.length - 1 &&
+          dominoes[i + 1] === "L" &&
+          (i === 0 || dominoes[i - 1] !== "R")
+        ) {
+          newDominoes[i] = "L";
+          changed = true;
+        }
+      }
+    }
+
+    dominoes = newDominoes.join("");
+  } while (changed);
+
+  return dominoes;
+}
+
+// Test the function
+console.log(simulateDominoes(".L.R....L")); // Should return "LL.RRRLLL"
+console.log(simulateDominoes("..R...L.L")); // Should return "..RR.LLLL"
